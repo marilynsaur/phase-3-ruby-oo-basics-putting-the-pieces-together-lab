4134250595d7251bcda695593581f1f24618ac56
Marilyn Wren <Marilynwren64@yahoo.com>
2022-09-20 14:11:04 -0400

    commit tree 38bcc1532e6ef0a3b6bd3b403009433e2658e76d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    attr_reader :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page
 
+  def initialize(title)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+    @turn_page = turn_page
+  
+   
+  end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :brand
+    attr_accessor :color
+    
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+  def initialize(brand)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  
+   
+  end
+end
+
